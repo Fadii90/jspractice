b4e8f17ec55eb4b689fb557dc2864411cd0eb82f
Fadii90 <122126348+Fadii90@users.noreply.github.com>
2024-03-24 17:24:45 +0500

    Add files via upload

--- callstack.js
+++ callstack.js
@@ -1,8 +1,22 @@
-let val1 = 10
+/*let val1 = 10
 let val2 = 5
 function addNum(num1, num2) {
     let total = num1 + num2
     return total
 }
 let result1 = addNum(val1, val2)
-let result2 = addNum(10, 2)
+let result2 = addNum(10, 2)*/
+function one(){
+    console.log("one");
+    two()
+}
+function two(){
+    console.log("two");
+    three()
+}
+function three(){
+    console.log("three");
+}
+one()
+two()
+three()
